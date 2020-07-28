cee1deb99959c3ed98974881a5264dcd1eac3125
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2020-07-28 23:23:22 +0200

    Running remote elevated scripts requires wait

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,6 +23,6 @@ echo "$SSH_KNOWN_HOSTS" > "$SSH_PATH/known_hosts"
 (umask 077 && echo "$SSH_KEY" > "$SSH_PATH/id_ed25519")
 ssh-keygen -yf "$SSH_PATH/id_ed25519" > "$SSH_PATH/id_ed25519.pub"
 
-ssh "$SSH_USERNAME@$SSH_HOSTNAME" "Start-Process -Verb RunAs powershell '-File C:\Arma3\Stop-ArmaInstance.ps1 $ARMA_INSTANCE'"
+ssh "$SSH_USERNAME@$SSH_HOSTNAME" "Start-Process -Wait -Verb RunAs powershell '-File C:\Arma3\Stop-ArmaInstance.ps1 $ARMA_INSTANCE'"
 scp "$LOCAL_PATH" "$SSH_USERNAME@$SSH_HOSTNAME:$REMOTE_PATH"
-ssh "$SSH_USERNAME@$SSH_HOSTNAME" "Start-Process -Verb RunAs powershell '-Command Get-ScheduledTask $ARMA_INSTANCE | Start-ScheduledTask'"
+ssh "$SSH_USERNAME@$SSH_HOSTNAME" "Start-Process -Wait -Verb RunAs powershell '-Command Get-ScheduledTask $ARMA_INSTANCE | Start-ScheduledTask'"
