3c9dee59e7914ec3223f7af66eaba5c4c75e9585
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2020-07-28 22:57:42 +0200

    Merge setup into entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM alpine
-COPY ./entrypoint.sh /
-COPY ./setup.sh /setup.sh
-RUN chmod +x /entrypoint.sh && chmod +x /setup.sh && apk add openssh-client=8.3_p1-r0
+COPY ./entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh && apk add openssh-client=8.3_p1-r0
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ if [ "$#" -ne "3" ]; then
   exit 1
 fi
 
+SSH_PATH=/root/.ssh
 LOCAL_PATH=$GITHUB_WORKSPACE/$1
 REMOTE_PATH=$3
 ARMA_INSTANCE=$3
@@ -14,6 +15,14 @@ if [ ! -f "$LOCAL_PATH" ]; then
   exit 2
 fi
 
-ssh -vvv "$SSH_USERNAME@$SSH_HOSTNAME" "Start-Process -Verb RunAs powershell '-File C:\Arma3\Stop-ArmaInstance.ps1 $ARMA_INSTANCE'"
-scp -vvv "$LOCAL_PATH" "$SSH_USERNAME@$SSH_HOSTNAME:$REMOTE_PATH"
-ssh -vvv "$SSH_USERNAME@$SSH_HOSTNAME" "Start-Process -Verb RunAs powershell '-Command Get-ScheduledTask $ARMA_INSTANCE | Start-ScheduledTask'"
+if [ ! -d "$SSH_PATH" ]; then
+  mkdir --parents "$SSH_PATH"
+fi
+
+echo "$SSH_KNOWN_HOSTS" > "$SSH_PATH/known_hosts"
+(umask 077 && echo "$SSH_KEY" > "$SSH_PATH/id_ed25519")
+ssh-keygen -yf "$SSH_PATH/id_ed25519" > "$SSH_PATH/id_ed25519.pub"
+
+ssh "$SSH_USERNAME@$SSH_HOSTNAME" "Start-Process -Verb RunAs powershell '-File C:\Arma3\Stop-ArmaInstance.ps1 $ARMA_INSTANCE'"
+scp "$LOCAL_PATH" "$SSH_USERNAME@$SSH_HOSTNAME:$REMOTE_PATH"
+ssh "$SSH_USERNAME@$SSH_HOSTNAME" "Start-Process -Verb RunAs powershell '-Command Get-ScheduledTask $ARMA_INSTANCE | Start-ScheduledTask'"
