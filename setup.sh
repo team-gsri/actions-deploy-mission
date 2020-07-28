d84822686124a86b3eb690b89ef024fff814ce2d
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2020-07-28 22:44:55 +0200

    Fix ssh paths

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-echo "$SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts
-echo "$SSH_KEY" > ~/.ssh/id_ed25519 && chmod 400 ~/.ssh/id_ed25519
-ssh-keygen -y -f ~/.ssh/id_ed25519 > ~/.ssh/id_ed25519.pub
+SSH_PATH=/root/.ssh
+
+if [ ! -d "$SSH_PATH" ]; then
+  mkdir --parents "$SSH_PATH"
+fi
+
+echo "$SSH_KNOWN_HOSTS" > "$SSH_PATH/known_hosts"
+(umask 077 && echo "$SSH_KEY" > "$SSH_PATH/id_ed25519")
+ssh-keygen -yf "$SSH_PATH/id_ed25519" > "$SSH_PATH/id_ed25519.pub"
